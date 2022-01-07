cd879b43fec63be9550a0f95e0710e35867f9af8
Jaskirat Singh <jsmokha33@gmail.com>
2022-01-07 23:55:20 +0530

    generic tree constructor added

---
+++ LEVEL 1/Basic Data Structures/Generic Trees/generic_tree_constructor.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class GenericTreeConstruction {
+
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static Node constructGenericTree(int[] arr) {
+        Node root = null;
+        Stack<Node> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == -1) {
+                stack.pop();
+            } else {
+
+                Node temp = new Node();
+                temp.data = arr[i];
+
+                if (stack.isEmpty()) {
+                    root = temp;
+                } else {
+                    stack.peek().children.add(temp);
+                }
+                stack.push(temp);
+            }
+        }
+        return root;
+    }
+
+    public static void displayGenericTree(Node node){
+        StringBuilder sb = new StringBuilder(new String(node.data + " -> "));
+        for(Node child: node.children){
+            sb.append(child.data + ", ");
+        }
+        sb.append(".");
+        System.out.println(sb);
+        for(Node child: node.children){
+            displayGenericTree(child);
+        }
+    }
+
+    public static void main(String[] args) { // Driver Program
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // int n = Integer.parseInt(br.readLine());
+        // int[] treeNodesInfo = new int[n];
+        // for (int i = 0; i < treeNodesInfo.length; i++) {
+        // treeNodesInfo[i] = Integer.parseInt(br.readLine());
+        // }
+        int[] treeNodesInfo = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100,
+                -1, -1, -1 };
+        Node root = constructGenericTree(treeNodesInfo);
+        displayGenericTree(root);
+    }
+}
